e99a151073a260595a24a7344316193bbbe0e8fc
Andrew Janke <andrew@apjanke.net>
2015-11-30 21:11:45 -0500

    vi-mode: Bind more cursor keys in vi mode

--- plugins/vi-mode/vi-mode.plugin.zsh
+++ plugins/vi-mode/vi-mode.plugin.zsh
@@ -28,6 +28,36 @@ bindkey '^?' backward-delete-char
 bindkey '^h' backward-delete-char
 bindkey '^w' backward-kill-word
 
+# Runs a bindkey command for both vi keymaps
+function bindkey_both_vi() {
+  bindkey -M viins "$@"
+  bindkey -M vicmd "$@"
+}
+
+# Navigation keys
+if [[ "${terminfo[kpp]}" != "" ]]; then
+  bindkey_both_vi "${terminfo[kpp]}" up-line-or-history       # [PageUp] - Up a line of history
+fi
+if [[ "${terminfo[knp]}" != "" ]]; then
+  bindkey_both_vi "${terminfo[knp]}" down-line-or-history     # [PageDown] - Down a line of history
+fi
+if [[ "${terminfo[khome]}" != "" ]]; then
+  bindkey_both_vi "${terminfo[khome]}" beginning-of-line      # [Home] - Go to beginning of line
+fi
+if [[ "${terminfo[kend]}" != "" ]]; then
+  bindkey_both_vi "${terminfo[kend]}"  end-of-line            # [End] - Go to end of line
+fi
+bindkey_both_vi '^[[1;5C' forward-word                        # [Ctrl-RightArrow] - move forward one word
+bindkey_both_vi '^[[1;5D' backward-word                       # [Ctrl-LeftArrow] - move backward one word
+if [[ "${terminfo[kdch1]}" != "" ]]; then
+  bindkey_both_vi "${terminfo[kdch1]}" delete-char            # [Delete] - delete forward
+else
+  bindkey_both_vi "^[[3~" delete-char
+  bindkey_both_vi "^[3;5~" delete-char
+  bindkey_both_vi "\e[3~" delete-char
+fi
+
+
 # if mode indicator wasn't setup by theme, define default
 if [[ "$MODE_INDICATOR" == "" ]]; then
   MODE_INDICATOR="%{$fg_bold[red]%}<%{$fg[red]%}<<%{$reset_color%}"
